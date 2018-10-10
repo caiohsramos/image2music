18c5891cdc0217322bf9be58aecf82530c93efb6
Caio Ramos <caioramos97@gmail.com>
2018-10-10 14:25:33 -0300

    adding redux logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './main/App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './main/reducers'
 import * as serviceWorker from './serviceWorker';
 require('dotenv').config()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(reducers,devTools)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/main/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <h1>App</h1>
+      </Router>
+    );
+  }
+}
+
+export default App;
