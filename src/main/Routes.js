96ba889bc7e279022302b5ba4593e4b936ee40e8
Caio Ramos <caioramos97@gmail.com>
2018-10-11 20:42:48 -0300

    creating imageMusic route

--- src/main/Routes.js
+++ src/main/Routes.js
@@ -3,12 +3,14 @@ import { Route, Redirect, Switch } from 'react-router-dom'
 import About from '../about/About'
 import Home from '../home/Home';
 import LoadImage from '../loadImage/LoadImage';
+import ImageMusic from '../imageMusic/imageMusic';
 
 export default props => (
     <Switch>
         <Route exact path='/about' component={About}/>
         <Route exact path='/load-image' component={LoadImage}/>
         <Route exact path='/' component={Home} />
+        <Route exact path='/image-music' component={ImageMusic} />
         <Redirect from='*' to='/' />
     </Switch>
 )
