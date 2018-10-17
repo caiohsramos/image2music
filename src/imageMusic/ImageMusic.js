90e54d7d4765840277a5606cf792a6d1c9916904
Caio Ramos <caioramos97@gmail.com>
2018-10-17 18:46:11 -0300

    adding properties component

--- src/imageMusic/ImageMusic.js
+++ src/imageMusic/ImageMusic.js
@@ -5,6 +5,8 @@ import PageHeader from 'react-bootstrap/lib/PageHeader'
 import Grid from 'react-bootstrap/lib/Grid'
 import { getProperties } from './watsonVision'
 import { getSongs } from './spotifySearch'
+import MusicList from './MusicList'
+import PropertiesList from './PropertiesList'
 
 export default class ImageMusic extends React.Component {
     constructor(props) {
@@ -16,15 +18,15 @@ export default class ImageMusic extends React.Component {
             tracks: [],
         }
         this.url = qs.parse(props.location.search).url
-        getProperties(this.url,this.handleResults)
+        getProperties(this.url, this.handleResults)
     }
 
     handleResults = (resp) => {
-        const classes = resp.data.images[0].classifiers[0].classes 
+        const classes = resp.data.images[0].classifiers[0].classes
         console.log(classes)
         this.setState({
-            ...this.state, 
-            isLoadingWatson: false, 
+            ...this.state,
+            isLoadingWatson: false,
             classes: classes
         })
         getSongs(classes, this.handleSongs)
@@ -34,35 +36,24 @@ export default class ImageMusic extends React.Component {
         const tracks = resp.data.tracks.items
         console.log(tracks)
         this.setState({
-            ...this.state, 
-            isLoadingSpotify: false, 
+            ...this.state,
+            isLoadingSpotify: false,
             tracks: tracks,
         })
     }
 
-    renderClasses = () => (
-        this.state.classes.map((obj, index) =>(
-            <h4 key={index}>{obj.class}{' '}{obj.score}</h4>
-        ))
-    )
-
-    renderSongs = () => (
-        this.state.tracks.map((obj, index) =>(
-            <h4 key={index}>{obj.name}</h4>
-        ))
-    )
-
     render() {
         return (
             <Grid>
-                {!this.url && <Redirect to='/'/>}
+                {!this.url && <Redirect to='/' />}
                 <PageHeader>
                     Results <small>{this.url}</small>
                 </PageHeader>
-                {this.state.isLoadingWatson ? <h2>Loading classes...</h2> : this.renderClasses()}
+                {this.state.isLoadingWatson ? <h2>Loading classes...</h2> :
+                    <PropertiesList classes={this.state.classes} />}
                 <hr />
                 {(this.state.isLoadingSpotify)
-                    ? <h2>Loading songs...</h2> : this.renderSongs()}
+                    ? <h2>Loading songs...</h2> : <MusicList tracks={this.state.tracks} />}
             </Grid>
         )
     }
---
+++ src/imageMusic/PropertiesList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ListGroup from 'react-bootstrap/lib/ListGroup'
+import ListGroupItem from 'react-bootstrap/lib/ListGroupItem'
+import Collapse from 'react-bootstrap/lib/Collapse'
+
+export default class PropertiesList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: false,
+        }
+    }
+
+    toggleOpen = () => {
+        this.setState({ ...this.state, isOpen: !this.state.isOpen })
+    }
+
+
+    render() {
+        return (
+            <ListGroup>
+                <ListGroupItem active onClick={this.toggleOpen}>Properties</ListGroupItem>
+                <Collapse in={this.state.isOpen}>
+                    <div>
+                        {this.props.classes.map((obj, index) => (
+                            <ListGroupItem key={index}>
+                                {obj.class}{' '}{obj.score}
+                            </ListGroupItem>
+                        ))}
+                    </div>
+                </Collapse>
+            </ListGroup>
+        )
+    }
+}
